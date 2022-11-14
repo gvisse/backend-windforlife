b47bdd790f8bfe4a805d7b1082baff2a478285e3
Guillaume Visse <guillaume.visse@gmail.com>
2022-11-14 11:03:01 +0100

    Create Anemometer viewset

--- WindForLife/anemometer/views.py
+++ WindForLife/anemometer/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Anemometer
+from .serializers import AnemometerSerializer
 
-# Create your views here.
+from rest_framework import viewsets, permissions
+
+class AnemometerViewSet(viewsets.ModelViewSet):
+
+    serializer_class = AnemometerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Anemometer.objects.all().prefetch_related('tags')
+        return queryset
