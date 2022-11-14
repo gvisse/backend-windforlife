636337660bf60e9b5d9bc676e885867dbf24180a
Guillaume Visse <guillaume.visse@gmail.com>
2022-11-14 11:31:13 +0100

    Add Wind model

--- WindForLife/wind/models.py
+++ WindForLife/wind/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from anemometer.models import Anemometer
+
+class Wind(models.Model):
+
+    class Meta:
+        verbose_name = 'Mesure de vitesse du vent'
+        unique_together = ['speed', 'time', 'anemometer']
+
+    speed = models.FloatField(verbose_name='vitesse (en nœuds)')
+    time = models.DateTimeField(verbose_name='date de la mesure')
+    # if an anemometer is deleted, all associated wind measurements are deleted
+    anemometer = models.ForeignKey(Anemometer, on_delete=models.CASCADE, related_name='winds')
+
+    def __str__(self):
+        return f'{self.anemometer.name} : {self.speed} à {self.time}'
