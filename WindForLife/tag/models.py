2e71d2a10f20e1356b8f1086881486ceb17c49ad
Guillaume Visse <guillaume.visse@gmail.com>
2022-11-14 10:22:42 +0100

    Create model Tag

--- WindForLife/tag/models.py
+++ WindForLife/tag/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+
+    class Meta:
+        verbose_name = 'Tag'
+
+    name = models.CharField(verbose_name='nom', max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
