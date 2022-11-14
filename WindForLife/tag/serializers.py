56326015848c33b425d55c14004cb44421f86ab8
Guillaume Visse <guillaume.visse@gmail.com>
2022-11-14 10:38:53 +0100

    Serialize model Tag

---
+++ WindForLife/tag/serializers.py
@@ -0,0 +1,11 @@
+from .models import Tag
+from rest_framework import serializers
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        extra_kwargs = {
+            'name': {'validators': []},
+        }
