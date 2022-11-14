7d2af85ae80558b4f7b54e0dfaaf26e66055599f
Guillaume Visse <guillaume.visse@gmail.com>
2022-11-14 10:31:37 +0100

    Setting db, adding rest_framework

--- WindForLife/WindForLife/settings.py
+++ WindForLife/WindForLife/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'tag'
 ]
 
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'WindForLife.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'WindForLife_db',
+        'USER': 'guillaume',
+        'PASSWORD': 'WindForLife_API_db',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
