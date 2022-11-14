72c41d77a0153a3b254332970600ea8863159304
Guillaume Visse <guillaume.visse@gmail.com>
2022-11-14 10:56:41 +0100

    Create Anemometer model

--- WindForLife/anemometer/models.py
+++ WindForLife/anemometer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from tag.models import Tag
+class Anemometer(models.Model):
 
-# Create your models here.
+    class Meta:
+        verbose_name = 'Anemomètre'
+        unique_together = ['latitude', 'longitude', 'altitude']
+
+    name = models.CharField(verbose_name='nom', max_length=255)
+    latitude = models.DecimalField(verbose_name='latitude', max_digits=9, decimal_places=7, null=False)
+    longitude = models.DecimalField(verbose_name='longitude', max_digits=9, decimal_places=7, null=False)
+    altitude = models.IntegerField(verbose_name='altitude(en m)', null=False)
+    tags = models.ManyToManyField(Tag, related_name='tags')
+
+    def __str__(self):
+        tags = ','.join([tag.name for tag in self.tags.all()])
+        return f'{self.name} ({tags}) : lat={self.latitude}, long={self.longitude}, alt={self.altitude}'
